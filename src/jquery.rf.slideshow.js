4e7de37feec23005875dd65b7f5d6b4b581cd4a9
Ryan Florence <rpflorence@gmail.com>
2012-05-05 21:27:56 -0600

    tabs -> spaces, added vars for each variable

--- src/jquery.rf.slideshow.js
+++ src/jquery.rf.slideshow.js
@@ -7,243 +7,243 @@
  *
 */
 
-(function ( jQuery ){
+;(function ( jQuery ){
 
-jQuery.widget('rf.slideshow', {
+  jQuery.widget('rf.slideshow', {
 
-	options: {
-		transition: 'fade',
-		selector: '> *',
-		initialIndex: 0,
-		autoStyle: true,
-		autoPlay: true,
-		delay: 3000,
-		duration: 400,
-		resetTimerOnShow: true
-	},
+    options: {
+      transition: 'fade',
+      selector: '> *',
+      initialIndex: 0,
+      autoStyle: true,
+      autoPlay: true,
+      delay: 3000,
+      duration: 400,
+      resetTimerOnShow: true
+    },
 
-	_create: function(){
-		if ( this.options.autoStyle ){
-			if ( this.element.css('position') === 'static' ) {
-				this.element.css( 'position', 'relative' );
-			}
-			this.element.css( 'overflow', 'hidden' );
-		}
-		this.transitioning = false;
-		this.playing = false;
+    _create: function(){
+      if ( this.options.autoStyle ){
+        if ( this.element.css('position') === 'static' ) {
+          this.element.css( 'position', 'relative' );
+        }
+        this.element.css( 'overflow', 'hidden' );
+      }
+      this.transitioning = false;
+      this.playing = false;
 
-		if ( this.options.autoPlay ) this.play();
-	},
+      if ( this.options.autoPlay ) this.play();
+    },
 
-	_init: function(){
-		this.slides = this.element.find( this.options.selector ).map(function( i, node ){
-			return jQuery( node );
-		});
-		this.current = this.current || this.options.initialIndex;
-		this._setupSlides();
-	},
+    _init: function(){
+      this.slides = this.element.find( this.options.selector ).map(function( i, node ){
+        return jQuery( node );
+      });
+      this.current = this.current || this.options.initialIndex;
+      this._setupSlides();
+    },
 
-	show: function( what, options ){
-		var index = ( typeof what === 'number' ) ? what : this['_' + what]();
-		if ( this.transitioning || this.current === index ) return;
+    show: function( what, options ){
+      var index = ( typeof what === 'number' ) ? what : this['_' + what]();
+      if ( this.transitioning || this.current === index ) return;
 
-		if (this.playing && this.options.resetTimerOnShow) this.stop().play();
+      if (this.playing && this.options.resetTimerOnShow) this.stop().play();
 
-		var opts = jQuery.extend( {}, this.options, options ),
-			trans = this._parseTransition( opts.transition ),
-			next = this._prepNext( index ),
-			prev = this._prepPrevious( this.current ),
-			eventData = {
-				previous: { element: prev, index: prev.data( 'slideshow:index' ) },
-				next: { element: next, index: index },
-				instance: this
-			};
+      var opts = jQuery.extend( {}, this.options, options );
+      var trans = this._parseTransition( opts.transition );
+      var next = this._prepNext( index );
+      var prev = this._prepPrevious( this.current );
+      var eventData = {
+        previous: { element: prev, index: prev.data( 'slideshow:index' ) },
+        next: { element: next, index: index },
+        instance: this
+      };
 
-		this.transitioning = true;
-		this.current = index;
+      this.transitioning = true;
+      this.current = index;
 
-		next.addClass( this.widgetBaseClass + '-next' );
-		prev.addClass( this.widgetBaseClass + '-previous' )
-			.removeClass( this.widgetBaseClass + '-current');
-		this.element.addClass( this.widgetBaseClass + '-transitioning' );
+      next.addClass( this.widgetBaseClass + '-next' );
+      prev.addClass( this.widgetBaseClass + '-previous' )
+          .removeClass( this.widgetBaseClass + '-current');
+      this.element.addClass( this.widgetBaseClass + '-transitioning' );
 
-		trans.args.unshift({
-			previous: prev,
-			next: next,
-			duration: opts.duration,
-			instance: this
-		});
+      trans.args.unshift({
+        previous: prev,
+        next: next,
+        duration: opts.duration,
+        instance: this
+      });
 
-		this._trigger( 'show', null, eventData );
-		_transitions[trans.name].apply( _transitions, trans.args );
+      this._trigger( 'show', null, eventData );
+      _transitions[trans.name].apply( _transitions, trans.args );
 
-		setTimeout(jQuery.proxy(function(){
-			this.transitioning = false;
-			next.removeClass( this.widgetBaseClass + '-next' )
-				.addClass( this.widgetBaseClass + '-current' );
-			prev.removeClass( this.widgetBaseClass + '-previous' )
-				.css( 'display', 'none' );
-			this.element.removeClass( this.widgetBaseClass + '-transitioning' );
-			this._trigger( 'complete', null, eventData );
-		}, this), opts.duration);
+      setTimeout(jQuery.proxy(function(){
+        this.transitioning = false;
+        next.removeClass( this.widgetBaseClass + '-next' )
+            .addClass( this.widgetBaseClass + '-current' );
+        prev.removeClass( this.widgetBaseClass + '-previous' )
+            .css( 'display', 'none' );
+        this.element.removeClass( this.widgetBaseClass + '-transitioning' );
+        this._trigger( 'complete', null, eventData );
+      }, this), opts.duration);
 
-		return this;
-	},
+      return this;
+    },
 
-	play: function( now ){
-		this._timer = setInterval(jQuery.proxy(function(){
-			this.show( 'next' );
-		}, this), this.options.delay);
-		if ( now ) this.show( 'next' );
-		this.playing = true;
-		return this;
-	},
+    play: function( now ){
+      this._timer = setInterval(jQuery.proxy(function(){
+        this.show( 'next' );
+      }, this), this.options.delay);
+      if ( now ) this.show( 'next' );
+      this.playing = true;
+      return this;
+    },
 
-	stop: function(){
-		clearInterval( this._timer );
-		this.playing = false;
-		return this;
-	},
+    stop: function(){
+      clearInterval( this._timer );
+      this.playing = false;
+      return this;
+    },
 
-	// internal & private, no backwards compatibily promised, use/extend at own risk
-	_parseTransition: function( transition ){
-		var matches = transition.match( /(.+)(\((.+)\))|(.+)/ );
-		return matches[4] ? {
-			name: matches[4],
-			args: []
-		} : {
-			name: matches[1],
-			args: jQuery.map( matches[3].split( ',' ), function( arg ){
-				var trimmed = jQuery.trim( arg ),
-					parsed = parseFloat( trimmed );
-				if ( trimmed === 'true' ) return true;
-				if ( trimmed === 'false' ) return false;
-				if ( trimmed === '' || trimmed === 'undefined' ) return undefined;
-				if ( trimmed === 'null' ) return null;
-				if ( !isNaN( parsed ) ) return parsed;
-				return trimmed;
-			})
-		};
-	},
+    // internal & private, no backwards compatibily promised, use/extend at own risk
+    _parseTransition: function( transition ){
+      var matches = transition.match( /(.+)(\((.+)\))|(.+)/ );
+      return matches[4] ? {
+        name: matches[4],
+        args: []
+      } : {
+        name: matches[1],
+        args: jQuery.map( matches[3].split( ',' ), function( arg ){
+          var trimmed = jQuery.trim( arg );
+          var parsed = parseFloat( trimmed );
+          if ( trimmed === 'true' ) return true;
+          if ( trimmed === 'false' ) return false;
+          if ( trimmed === '' || trimmed === 'undefined' ) return undefined;
+          if ( trimmed === 'null' ) return null;
+          if ( !isNaN( parsed ) ) return parsed;
+          return trimmed;
+        })
+      };
+    },
 
-	_next: function(){
-		return this.current === this.slides.length - 1 ? 0 : this.current + 1;
-	},
+    _next: function(){
+      return this.current === this.slides.length - 1 ? 0 : this.current + 1;
+    },
 
-	_previous: function(){
-		return this.current === 0 ? this.slides.length - 1 : this.current - 1;
-	},
+    _previous: function(){
+      return this.current === 0 ? this.slides.length - 1 : this.current - 1;
+    },
 
-	_styles: { left: 0, top: 0, position: 'absolute' },
+    _styles: { left: 0, top: 0, position: 'absolute' },
 
-	_prepNext: function( i ){
-		var slide = this.slides[i],
-			style = slide.data( 'slideshow:style' ),
-			css = { display: '', 'z-index': 0 };
-		if ( this.options.autoStyle ) jQuery.extend( css, this._styles );
-		return slide.attr( 'style', style ).css( css );
-	},
+    _prepNext: function( i ){
+      var slide = this.slides[i];
+      var style = slide.data( 'slideshow:style' );
+      var css = { display: '', 'z-index': 0 };
+      if ( this.options.autoStyle ) jQuery.extend( css, this._styles );
+      return slide.attr( 'style', style ).css( css );
+    },
 
-	_prepPrevious: function( i ){
-		this._storeStyles( i );
-		return this.slides[i].css( 'z-index', 1 );
-	},
+    _prepPrevious: function( i ){
+      this._storeStyles( i );
+      return this.slides[i].css( 'z-index', 1 );
+    },
 
-	_storeStyles: function( i ){
-		this.slides[i].data( 'slideshow:style', this.slides[i].attr( 'style' ) || '' );
-	},
+    _storeStyles: function( i ){
+      this.slides[i].data( 'slideshow:style', this.slides[i].attr( 'style' ) || '' );
+    },
 
-	_setupSlides: function(){
-		for ( var i = 0, l = this.slides.length, slide; i < l; i++ ) {
-			slide = this.slides[i];
-			this._storeStyles( i );
-			slide.data( 'slideshow:index', i );
-			if ( this.options.autoStyle ) slide.css( this._styles );
-			slide.css( 'display', i === this.current ? '' : 'none' );
-		}
-	}
+    _setupSlides: function(){
+      for ( var i = 0, l = this.slides.length, slide; i < l; i++ ) {
+        slide = this.slides[i];
+        this._storeStyles( i );
+        slide.data( 'slideshow:index', i );
+        if ( this.options.autoStyle ) slide.css( this._styles );
+        slide.css( 'display', i === this.current ? '' : 'none' );
+      }
+    }
 
-});
+  });
 
-// transition definition "static" methods
-var _transitions = {};
+  // transition definition "static" methods
+  var _transitions = {};
 
-jQuery.rf.slideshow.defineTransition = function( name, transition ){
-	_transitions[name] = transition;
-};
+  jQuery.rf.slideshow.defineTransition = function( name, transition ){
+    _transitions[name] = transition;
+  };
 
-jQuery.rf.slideshow.defineTransitions = function( transitions ){
-	jQuery.each( transitions, function ( name, transition ){
-		jQuery.rf.slideshow.defineTransition( name, transition );
-	});
-};
+  jQuery.rf.slideshow.defineTransitions = function( transitions ){
+    jQuery.each( transitions, function ( name, transition ){
+      jQuery.rf.slideshow.defineTransition( name, transition );
+    });
+  };
 
-// references instead of conditionals
-var propMap = { left: 'left', right: 'left', up: 'top', down: 'top' },
-	valueMap = { left: '100%', up: '100%', right: '-100%', down: '-100%' },
-	invertValueMap = { left: '-100%', up: '-100%', right: '100%', down: '100%' };
+  // references for flow control
+  var propMap = { left: 'left', right: 'left', up: 'top', down: 'top' };
+  var valueMap = { left: '100%', up: '100%', right: '-100%', down: '-100%' };
+  var invertValueMap = { left: '-100%', up: '-100%', right: '100%', down: '100%' };
 
-// default transitions
-jQuery.rf.slideshow.defineTransitions({
+  // default transitions
+  jQuery.rf.slideshow.defineTransitions({
 
-	none: function( params ){
-		params.previous.css( 'display', 'none' );
-	},
+    none: function( params ){
+      params.previous.css( 'display', 'none' );
+    },
 
-	fade: function( params ){
-		params.previous.fadeOut( params.duration );
-	},
+    fade: function( params ){
+      params.previous.fadeOut( params.duration );
+    },
 
-	crossFade: function ( params ){
-		params.next.hide().fadeIn( params.duration );
-		params.previous.fadeOut( params.duration );
-	},
+    crossFade: function ( params ){
+      params.next.hide().fadeIn( params.duration );
+      params.previous.fadeOut( params.duration );
+    },
 
-	fadeThroughBackground: function ( params ){
-		var half = params.duration / 2;
-		params.next.hide();
-		params.previous.fadeOut( half );
-		setTimeout(function (){
-			params.next.fadeIn( half );
-		}, half);
-	},
+    fadeThroughBackground: function ( params ){
+      var half = params.duration / 2;
+      params.next.hide();
+      params.previous.fadeOut( half );
+      setTimeout(function (){
+        params.next.fadeIn( half );
+      }, half);
+    },
 
-	blind: function( params, direction, fade, ease ){
-		var animation = {},
-			css = { 'z-index': 2 },
-			prop = propMap[direction];
+    blind: function( params, direction, fade, ease ){
+      var animation = {};
+      var css = { 'z-index': 2 };
+      var prop = propMap[direction];
 
-		animation[prop] = '0%';
-		css[prop] = valueMap[direction];
-		params.next.css( css ).animate( animation, params.duration, ease );
-		if (fade) this.fade.apply( this, arguments );
-	},
+      animation[prop] = '0%';
+      css[prop] = valueMap[direction];
+      params.next.css( css ).animate( animation, params.duration, ease );
+      if (fade) this.fade.apply( this, arguments );
+    },
 
-	slide: function ( params, direction, fade, ease ){
-		var animation = {},
-			prop = propMap[direction];
-		animation[prop] = invertValueMap[direction];
-		if (fade) animation.opacity = 0;
-		params.previous.animate( animation, params.duration, ease );
-	},
+    slide: function ( params, direction, fade, ease ){
+      var animation = {};
+      var prop = propMap[direction];
+      animation[prop] = invertValueMap[direction];
+      if (fade) animation.opacity = 0;
+      params.previous.animate( animation, params.duration, ease );
+    },
 
-	push: function (params, direction, ease){
-		var nextAnimation = {},
-			prevAnimation = {},
-			css = { 'z-index': 2 },
-			prop = propMap[direction],
-			invert = ( direction === 'left' || direction === 'up' );
+    push: function (params, direction, ease){
+      var nextAnimation = {};
+      var prevAnimation = {};
+      var css = { 'z-index': 2 };
+      var prop = propMap[direction];
+      var invert = ( direction === 'left' || direction === 'up' );
 
-		nextAnimation[prop] = '0%';
-		css[prop] = valueMap[direction];
-		prevAnimation[prop] = invertValueMap[direction];
+      nextAnimation[prop] = '0%';
+      css[prop] = valueMap[direction];
+      prevAnimation[prop] = invertValueMap[direction];
 
-		params.next.css( css ).animate( nextAnimation, params.duration, ease );
-		params.previous.animate( prevAnimation, params.duration, ease );
-	}
+      params.next.css( css ).animate( nextAnimation, params.duration, ease );
+      params.previous.animate( prevAnimation, params.duration, ease );
+    }
 
-});
+  });
 
-jQuery.rf.slideshow.v = '1.1.0';
+  jQuery.rf.slideshow.v = '1.1.0';
 
 })(jQuery);
--- src/jquery.rf.slideshownav.js
+++ src/jquery.rf.slideshownav.js
@@ -6,61 +6,61 @@
  * http://ryanflorence.com/jquery-slideshow/
  *
 */
-(function (jQuery){
+;(function (jQuery){
 
-var _map = {
-	horizontal: { '<': 'left', '>': 'right'},
-	vertical:   { '<': 'up',   '>': 'down' }
-};
+  var _map = {
+    horizontal: { '<': 'left', '>': 'right'},
+    vertical:   { '<': 'up',   '>': 'down' }
+  };
 
-jQuery.widget( 'rf.slideshownav', jQuery.rf.slideshow, {
-	options: {
-		selector: '> div > *',
-		navSelector: '> ul > li > a',
-		navTransition: 'push(#{direction})',
-		mode: 'horizontal',
-		getTransition: function( index ){
-			var direction = this.current < index ? _map[this.options.mode]['<'] : _map[this.options.mode]['>'],
-				transition = this.options.navTransition.replace( /#\{direction\}/g, direction );
-			return { transition: transition };
-		},
-		autoPlay: false
-	},
+  jQuery.widget( 'rf.slideshownav', jQuery.rf.slideshow, {
+    options: {
+      selector: '> div > *',
+      navSelector: '> ul > li > a',
+      navTransition: 'push(#{direction})',
+      mode: 'horizontal',
+      getTransition: function( index ){
+        var direction = this.current < index ? _map[this.options.mode]['<'] : _map[this.options.mode]['>'];
+        var transition = this.options.navTransition.replace( /#\{direction\}/g, direction );
+        return { transition: transition };
+      },
+      autoPlay: false
+    },
 
-	_init: function(){
-		var self = this;
-		jQuery.rf.slideshow.prototype._init.apply( this, arguments ); // apply super
+    _init: function(){
+      var self = this;
+      jQuery.rf.slideshow.prototype._init.apply( this, arguments ); // apply super
 
-		this.navs = this.element.find( this.options.navSelector )
-			.map(function( index, node ){
-				var el = jQuery( node );
-				if (self.current === index) el.addClass( self.widgetBaseClass + '-current-nav' );
-				return el.bind( 'click.' + self.widgetEventPrefix, function( event ){
-					event.preventDefault();
-					self._trigger('click');
-					self.show( index, self.options.getTransition.call( self, index ) );
-				});
-			});
+      this.navs = this.element.find( this.options.navSelector )
+        .map(function( index, node ){
+          var el = jQuery( node );
+          if (self.current === index) el.addClass( self.widgetBaseClass + '-current-nav' );
+          return el.bind( 'click.' + self.widgetEventPrefix, function( event ){
+            event.preventDefault();
+            self._trigger('click', event);
+            self.show( index, self.options.getTransition.call( self, index ) );
+          });
+        });
 
-		this.element.bind({
-			'slideshownavshow': function( event, params ){
-				self.navs[params.next.index].addClass( params.instance.widgetBaseClass + '-next-nav' );
-				self.navs[params.previous.index]
-					.removeClass( params.instance.widgetBaseClass + '-current-nav' )
-					.addClass( params.instance.widgetBaseClass + '-previous-nav' );
-			},
+      this.element.bind({
+        'slideshownavshow': function( event, params ){
+          self.navs[params.next.index].addClass( params.instance.widgetBaseClass + '-next-nav' );
+          self.navs[params.previous.index]
+            .removeClass( params.instance.widgetBaseClass + '-current-nav' )
+            .addClass( params.instance.widgetBaseClass + '-previous-nav' );
+        },
 
-			'slideshownavcomplete': function( event, params ){
-				self.navs[params.next.index]
-					.removeClass( params.instance.widgetBaseClass + '-next-nav' )
-					.addClass( params.instance.widgetBaseClass + '-current-nav' );
-				self.navs[params.previous.index]
-					.removeClass( params.instance.widgetBaseClass + '-previous-nav' );
-				
-			}
-		});
-	}
+        'slideshownavcomplete': function( event, params ){
+          self.navs[params.next.index]
+            .removeClass( params.instance.widgetBaseClass + '-next-nav' )
+            .addClass( params.instance.widgetBaseClass + '-current-nav' );
+          self.navs[params.previous.index]
+            .removeClass( params.instance.widgetBaseClass + '-previous-nav' );
+        
+        }
+      });
+    }
 
-});
+  });
 
 })(jQuery);
